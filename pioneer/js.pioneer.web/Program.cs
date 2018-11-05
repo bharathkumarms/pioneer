ba5c3563d192dcf5d9b5fdeb48e76a5d0315a61f
JAYASANKARAM\vbhar <v.bharathkumar@gmail.com>
2018-11-05 20:12:51 +0530

    Initial Web project

--- pioneer/js.pioneer.web/Program.cs
+++ pioneer/js.pioneer.web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace js.pioneer.webproj
+namespace js.pioneer.web
 {
     public class Program
     {
