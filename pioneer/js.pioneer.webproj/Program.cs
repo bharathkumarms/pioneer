5de9a405b4fd0b6a3ca670379d32a07a340c0318
JAYASANKARAM\vbhar <v.bharathkumar@gmail.com>
2018-11-05 15:53:58 +0530

    Added webproj

--- pioneer/js.pioneer.webproj/Program.cs
+++ pioneer/js.pioneer.webproj/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace js.pioneer.web
+namespace js.pioneer.webproj
 {
     public class Program
     {
